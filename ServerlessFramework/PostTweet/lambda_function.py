980a7cb379dd88351b247927ccd8feb9e42dcda4
contradiction29 <sora32127@gmail.com>
2024-12-14 17:38:46 +0900

    Feature: Add tweet-id save process

--- ServerlessFramework/PostTweet/lambda_function.py
+++ ServerlessFramework/PostTweet/lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import supabase
 import tweepy
 import requests
 import logging
@@ -12,7 +13,7 @@ def get_twitter_credentials():
     secrets = json.loads(secret_value["SecretString"])
     return secrets
 
-def post_tweet(post_text, secrets):
+def post_tweet(post_text, secrets) -> str:
     consumer_key = secrets["CK"]
     consumer_secret = secrets["CS"]
     access_token = secrets["AT"]
@@ -35,6 +36,8 @@ def post_tweet(post_text, secrets):
         filename="/tmp/og_image.jpg"
     )  # apiv1とv2を併用している
     tweet = client.create_tweet(text=post_text, media_ids=[media.media_id])
+    tweet_id = tweet.data["id"]
+    return tweet_id
 
 def download_image(url):
     response = requests.get(url).content
@@ -46,7 +49,8 @@ def get_infomation_from_message(message):
     post_url = message["post_url"]
     og_url = message["og_url"]
     message_type = message["message_type"]
-    return post_title, post_url, og_url, message_type
+    post_id = message["post_id"]
+    return post_title, post_url, og_url, message_type, post_id
 
 def create_post_text(post_title, post_url, message_type):
     type_prefix = {
@@ -59,16 +63,33 @@ def create_post_text(post_title, post_url, message_type):
     
     return f"[{type_prefix[message_type]}] : {post_title} 健常者エミュレータ事例集\n{post_url}"
 
+def get_credentials_of_db():
+    secretmanager_client = boto3.client("secretsmanager")
+    secret_value = secretmanager_client.get_secret_value(SecretId="SUPABASE_CONNECTION_SECRET")
+    secrets = json.loads(secret_value["SecretString"])
+    return secrets
+
+
+def save_tweet_id_to_db(tweet_id, message_type, post_id):
+    if message_type != "new":
+        logger.info(f"message_type is {message_type}, not new. tweet_id: {tweet_id} is not saved to db.")
+        return
+    credentials_of_db = get_credentials_of_db()
+    client = supabase.create_client(credentials_of_db["SUPABASE_URL"], credentials_of_db["SUPABASE_SERVICE_ROLE_KEY"])
+    client.table("dim_posts").update({"tweet_id_of_first_tweet": tweet_id}).eq("post_id", post_id).execute()
+    logger.info(f"tweet_id: {tweet_id} is saved to db. post_id: {post_id}")
+    return
+
 def lambda_handler(event, context):
     try:
         message = json.loads(event["Records"][0]["Sns"]["Message"])
-        post_title, post_url, og_url, message_type = get_infomation_from_message(message)
+        post_title, post_url, og_url, message_type, post_id = get_infomation_from_message(message)
         secrets = get_twitter_credentials()
         download_image(og_url)
         post_text = create_post_text(post_title, post_url, message_type)
-        post_tweet(post_text, secrets)
-        logger.setLevel("INFO")
-        logger.info(f"post_title: {post_title} is successfully tweeted.")
+        tweet_id = post_tweet(post_text, secrets)
+        save_tweet_id_to_db(tweet_id, message_type, post_id)
+        logger.info(f"post_title: {post_title} is successfully tweeted. tweet_id: {tweet_id}")
     except Exception as e:
         logger.setLevel("ERROR")
         logger.error(e)
@@ -85,7 +106,8 @@ if __name__ == "__main__":
                             "post_title": "無神論者の火",
                             "post_url": "https://healthy-person-emulator.org/archives/23576",
                             "og_url": "https://healthy-person-emulator-public-assets.s3-ap-northeast-1.amazonaws.com/23576.jpg",
-                            "message_type": "new"
+                            "message_type": "new",
+                            "post_id": 23576
                         }
                     )
                 }
